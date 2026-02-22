978d7110e0cd268a3236ed690a4abea8a08e86f5
lolikit123 <lolikit.wasaznik@gmail.com>
2026-02-22 20:50:44 +0100

    Code clean and fix ci-build.yml file

--- scripts/build_relase.sh
+++ scripts/build_relase.sh
@@ -1,7 +1,8 @@
 set -euo pipefail
 
 make -C stm32/third_party/libopencm3 TARGETS=stm32/f4 -j"$(nproc)"
-cmake -S stm32/app -B stm32/app/buildR -DCMAKE_BUILD_TYPE=Release \
+cmake -S stm32/app -B stm32/app/buildR \
+        -DCMAKE_BUILD_TYPE=Release \
         -DAPP_ENABLE_TIMEBASE_TESTS=OFF \
         -DAPP_BUILD_MASTER=ON \
         -DAPP_BUILD_SLAVE=ON
