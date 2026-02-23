1076ac9b5ef42186be18b92536ecfaebea9df352
lolikit123 <lolikit.wasaznik@gmail.com>
2026-02-23 17:28:15 +0100

    Add builds for master/slave and change test too wroking properly

--- scripts/build_debug_tests.sh
+++ scripts/build_debug_tests.sh
@@ -1,5 +1,9 @@
 set -euo pipefail
 
 make -C stm32/third_party/libopencm3 TARGETS=stm32/f4 -j"$(nproc)"
-cmake -S stm32/app -B stm32/app/buildD -DCMAKE_BUILD_TYPE=Debug -DAPP_ENABLE_TIMEBASE_TESTS=ON
+cmake -S stm32/app -B stm32/app/buildD \
+  -DCMAKE_BUILD_TYPE=Debug \
+  -DAPP_ENABLE_TIMEBASE_TESTS=ON \
+  -DAPP_BUILD_MASTER=ON \
+  -DAPP_BUILD_SLAVE=ON
 cmake --build stm32/app/buildD -j"$(nproc)"
